1fcc5a196f5c1811a8223b9dd667a956bc96975b
Pedro <ponsvpedro@gmail.com>
2024-04-01 15:50:01 +0200

    added paths and modified lomb plot

--- src/class_lomb.py
+++ src/class_lomb.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from lib import lomb_periodogram as lp
 from lib import TJII_mirnov_array as tma
 from .class_linked_rois import Region
+from . import paths
 
 
 class Lomb:
@@ -14,6 +15,7 @@ class Lomb:
     ):
         self.coilarr = coilarr
         self.roilist = roilist
+        self.coilarr.polarr.get_calfacs(paths.POLOIDAL_CALIBRATION_PATH)
 
     def make_lomb(self):
         for roi in self.roilist:
@@ -29,12 +31,11 @@ class Lomb:
             tlim=roi.tlim, flim=roi.flim
         )
 
-        plt.plot(times, signals + 10 * thetas, "-", ms=1, lw=0.1)
-
         mapa = lp.easylomb3_difftimes(
             times, thetas, phis, signals, roi.f0, NS, MS
         )
-        fig, ax = plt.subplots(1, 1)
-        title = f"Shot {self.coilarr.shot}, poloidal, f0 = {roi.f0} kHz"
-        lp.plotmapa_alone_new(mapa, NS, MS, ax, title=title)
+        fig, ax = plt.subplots(1, 2)
+        title = f"Shot {self.coilarr.shot}, poloidal, f0 = {roi.f0:.2f} kHz, tlim=({roi.tlim[0]:.2f}, {roi.tlim[1]:.2f})"
+        ax[0].plot(times, signals + 10 * thetas, "-", ms=1, lw=0.1)
+        lp.plotmapa_alone_new(mapa, NS, MS, ax[1], title=title)
         plt.show()
--- src/paths.py
+++ src/paths.py
@@ -1,3 +1,4 @@
 DMUSIC_PATH = "/home/pedro/MEGA/00_doctorado/research/experiments/2022_spatial_periodicity_nbi_driven_ae/Analysis/DMUSIC/hdfs"
 POLOIDAL_CALIBRATION_PATH = "/home/pedro/MEGA/00_doctorado/research/experiments/2022_spatial_periodicity_nbi_driven_ae/Analysis/DATA/calibration/poloidal_calibration.yaml"
 DATA_PATH = "/home/pedro/MEGA/00_doctorado/research/experiments/2022_spatial_periodicity_nbi_driven_ae/Analysis/DATA/hdfs"
+LOMB_OUTPUT_PATH = "/home/pedro/MEGA/00_doctorado/research/experiments/2022_spatial_periodicity_nbi_driven_ae/Analysis/LOMB/figs"
