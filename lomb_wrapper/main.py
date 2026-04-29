c7ce2e5448cf4ebfb3c9de583e2de1038227336f
Pedro <ponsvpedro@gmail.com>
2026-04-29 22:17:27 +0200

    Add ctrl+c to close in terminal

--- lomb_wrapper/main.py
+++ lomb_wrapper/main.py
@@ -1,9 +1,13 @@
+import signal
 import sys  # We need sys so that we can pass argv to QApplication
 
 import matplotlib.pyplot as plt
 from PySide6.QtWidgets import QApplication
 
 from .main_window import MainWindow
+
+# Makes it possible to close the window with Ctrl+C in the terminal
+signal.signal(signal.SIGINT, signal.SIG_DFL)
 
 
 def main():
